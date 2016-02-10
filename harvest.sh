4d910b523bb9be35ea5d3fa0bf050de6f066b00f
Cole Hudson <colehudson@gmail.com>
2016-02-10 13:10:30 -0500

    Update and rename prune.sh to harvest.sh
    Now generates a tar.gz file for upload as a datastream to DigitalCommons site datastream.
    Fixed issue where input folder needed for dcArchive2Solr.py was not being created or updated.

---
+++ harvest.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+DCFILE=''
+
+while getopts 'f:*' flag; do
+	case "${flag}" in
+		f) DCFILE="${OPTARG}" ;;
+		*) error "Unexpected option ${flag}" ;;
+	esac
+done
+
+# untar file
+tar -xvf $DCFILE
+DCFOLDER=`tar -tzf $DCFILE | head -1 | cut -f1 -d"/"`
+
+# remove all the pdfs
+find $DCFOLDER -type f \! -regex '.*\.xml' -delete
+
+# put resulting directory into a compressed file
+tar -cvf digitalcommons.tar.gz $DCFOLDER
+
+# remove input directory
+DIRECTORY='./input'
+if [ -d "$DIRECTORY" ]; then
+  rm -r $DIRECTORY
+fi
+
+# remake input directory
+mv $DCFOLDER $DIRECTORY
