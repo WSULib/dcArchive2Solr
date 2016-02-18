156dbc30a77bd17202a6e7874b46e81d4c23d749
Cole Hudson <colehudson@gmail.com>
2016-02-18 11:49:34 -0500

    Update harvest.sh

--- harvest.sh
+++ harvest.sh
@@ -2,28 +2,33 @@
 
 DCFILE=''
 
-while getopts 'f:*' flag; do
-	case "${flag}" in
-		f) DCFILE="${OPTARG}" ;;
-		*) error "Unexpected option ${flag}" ;;
+while getopts ':f:' flag; do
+	case $flag in
+		f) DCFILE="${OPTARG}" >&2 ;;
+		\?) echo "Option -$OPTARG requires an argument." >&2 exit 1 ;;
+		:) error "Unexpected option ${flag}" >&2 exit 1 ;;
 	esac
 done
 
-# untar file
-tar -xvf $DCFILE
-DCFOLDER=`tar -tzf $DCFILE | head -1 | cut -f1 -d"/"`
 
-# remove all the pdfs
-find $DCFOLDER -type f \! -regex '.*\.xml' -delete
+if [ -n "${DCFILE}" ]; then
 
-# put resulting directory into a compressed file
-tar -cvf digitalcommons.tar.gz $DCFOLDER
+	# untar file
+	tar -xvf $DCFILE
+	DCFOLDER=`tar -tzf $DCFILE | head -1 | cut -f1 -d"/"`
 
-# remove input directory
-# DIRECTORY='./input'
-# if [ -d "$DIRECTORY" ]; then
-#   rm -r $DIRECTORY
-# fi
+	# remove all the pdfs
+	find $DCFOLDER -type f \! -regex '.*\.xml' -delete
 
-# remake input directory
-mv $DCFOLDER $DIRECTORY
+	# put resulting directory into a compressed file
+	tar -cvf digitalcommons.tar.gz $DCFOLDER
+
+	# remove input directory
+	# DIRECTORY='./input'
+	# if [ -d "$DIRECTORY" ]; then
+	#   rm -r $DIRECTORY
+	# fi
+
+	# remake input directory
+	mv $DCFOLDER $DIRECTORY
+fi
